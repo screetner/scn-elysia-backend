4d2994ca91a21747b1cd7600bc40cfe80d697f1f
AuJung <nuttapong6280@gmail.com>
2024-08-07 11:17:59 +0700

    feat: allow cors

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,10 @@ import { swagger } from '@elysiajs/swagger'
 import {auth, sample, geolocation} from "@/routes";
 import {jwt} from "@elysiajs/jwt";
 import { Logestic } from 'logestic';
+import cors from "@elysiajs/cors";
 
 const app = new Elysia()
     .use(Logestic.preset('common'))
-    .get("/", () => "Hello Elysia")
-    .use(auth)
-    .use(geolocation)
-    .use(sample)
     .use(jwt({
         name: "jwt",
         secret: process.env.JWT_SECRET || "secret"
@@ -22,4 +19,12 @@ const app = new Elysia()
             ]
         }
     }))
+    .use(cors({
+        origin: "*",
+        methods: ["GET", "POST", "PATCH", "DELETE"],
+    }))
+    .use(auth)
+    .use(geolocation)
+    .use(sample)
+    .get("/", () => "Hello Elysia")
     .listen(3000);
