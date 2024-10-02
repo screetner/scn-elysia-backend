cdc4227325d01211949a0f1dafc3574f3c13c172
i am doraemon <khunball46@gmail.com>
2024-10-02 21:12:07 +0700

    - create new organization api (#35)

--- src/models/organization.ts
+++ src/models/organization.ts
@@ -1,6 +1,19 @@
+import {t} from "elysia";
+
 export interface organizationData {
     orgId: string,
     orgName: string,
     orgMember: number,
     orgAssets: number,
 }
+
+export interface createOrganizationResult {
+    orgId: string,
+    defaultRoleId: string,
+    adminRoleId: string,
+}
+
+export const createOrganizationBody = t.Object({
+    adminEmail: t.Array(t.String()),
+    orgName: t.String(),
+});
--- src/routes/organization/organization.ts
+++ src/routes/organization/organization.ts
@@ -1,12 +1,16 @@
 import {Elysia} from "elysia";
 import {checkAccessToken} from "@/middleware/jwtRequire";
-import {getAllOrganization} from "@/routes/organization/organization-service";
-import {organizationData} from "@/models/organization";
+import {createOrganization, getAllOrganization} from "@/routes/organization/organization-service";
+import {createOrganizationBody, organizationData} from "@/models/organization";
+import {addInviteToDatabase, checkEmailExist, sendInviteEmail} from "@/routes/member/member-service";
+import {sendInviteToken} from "@/models/member";
+import {jwtInviteSetup} from "@/routes/auth/setup";
 
 export const organization = (app: Elysia) =>
     app.group("organization", (app) => {
         return app
             .use(checkAccessToken)
+            .use(jwtInviteSetup)
             .get('/all', async ({error, payload}) => {
                 try {
                     if (!payload.isOwner) return error(401, "Unauthorized")
@@ -24,6 +28,33 @@ export const organization = (app: Elysia) =>
                     description: "Get Organization Information",
                     tags: ["Organization"]
                 }
+            })
+            .post('/create', async ({error, payload, body, jwtInvite}) => {
+                try {
+                    if (!payload.isOwner) return error(401, "Unauthorized")
+
+                    await checkEmailExist(body.adminEmail)
+                    const data = await createOrganization(body.orgName)
+
+                    const sendInviteTokens: sendInviteToken[] = [];
+                    await Promise.all(body.adminEmail.map(async email => {
+                        const token = await jwtInvite.sign({ email, orgId: data.orgId, roleId: data.adminRoleId });
+                        sendInviteTokens.push({ email, token });
+                    }));
+
+                    await Promise.all([
+                        addInviteToDatabase(payload.userId, data.orgId, sendInviteTokens.map(e => e.token)),
+                        sendInviteEmail(sendInviteTokens)
+                    ]);
+                } catch (e) {
+                    return error(500, e)
+                }
+            }, {
+                detail: {
+                    description: "Create Organization",
+                    tags: ["Organization"]
+                },
+                body: createOrganizationBody
             })
     },
     );
