685fdfaee849b76065cd733121167c6b0583330d
i am doraemon <khunball46@gmail.com>
2024-11-01 12:11:46 +0700

    - revert version

--- src/libs/emailclient.ts
+++ src/libs/emailclient.ts
@@ -1,11 +1,6 @@
-import { EmailClient } from "@azure/communication-email";
+import {EmailClient} from "@azure/communication-email";
 
-const connectionString = process.env.COMMUNICATION_SERVICES_CONNECTION_STRING;
+const connectionString = process.env['COMMUNICATION_SERVICES_CONNECTION_STRING'];
+const client = new EmailClient(connectionString!);
 
-if (!connectionString) {
-    throw new Error("COMMUNICATION_SERVICES_CONNECTION_STRING is not defined");
-}
-
-const client = new EmailClient(connectionString);
-
-export default client;
+export default client
