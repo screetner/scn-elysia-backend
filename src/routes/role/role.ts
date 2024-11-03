53d8a87c889234f090c91a9985680c79c91aef78
i am doraemon <khunball46@gmail.com>
2024-11-03 14:35:25 +0700

    44 get org role (#49)
    * - create new api
    
    * Update role.ts
    
    Hot fix
    
    * - change api path

--- src/routes/role/role.ts
+++ src/routes/role/role.ts
@@ -36,11 +36,13 @@ export const role = (app: Elysia) =>
                     tags: ["Role"]
                 }
             })
-            .get('/orgId', async ({error, payload}) => {
+            .get('/org/:orgId', async ({error, params, payload}) => {
                 try {
+                    const orgId = params.orgId;
+
                     if (!payload.isOwner) return error(401, "Unauthorized")
 
-                    const response: roleInOrg[] = await getRoleOrganization(payload!.orgId)
+                    const response: roleInOrg[] = await getRoleOrganization(orgId)
 
                     if (!response) return error(401, "Unauthorized")
 
