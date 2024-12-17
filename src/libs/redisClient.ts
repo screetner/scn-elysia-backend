726fb1dcc1b581bb9528e51f61b021e975cba6f7
AuJung <nuttapong6280@gmail.com>
2024-12-17 11:09:46 +0700

    fix: redis client

--- src/libs/redisClient.ts
+++ src/libs/redisClient.ts
@@ -1,5 +1,4 @@
-import Redis from 'ioredis'
-import * as process from 'node:process'
+import { Redis } from '@upstash/redis'
 
 class RedisSingleton {
   private static instance: Redis
@@ -7,7 +6,10 @@ class RedisSingleton {
   public static getInstance(): Redis {
     if (!RedisSingleton.instance) {
       // Initialize the Redis client only once
-      RedisSingleton.instance = new Redis(process.env.REDIS_URL!)
+      RedisSingleton.instance = new Redis({
+        url: process.env.REDIS_URL,
+        token: process.env.REDIS_TOKEN,
+      })
     }
     return RedisSingleton.instance
   }
