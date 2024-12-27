0cc0182287ffabb0a7942ad469c09b14220fefc1
i am doraemon <khunball46@gmail.com>
2024-12-27 14:14:48 +0700

    hot fix - enhance postGeo to can set to null value

--- src/routes/geolocation/geolocation-service.ts
+++ src/routes/geolocation/geolocation-service.ts
@@ -1,23 +1,24 @@
-import {db} from "@/database/database";
-import {eq} from "drizzle-orm";
-import {PostGeoBodyType} from "@/models/geolocation";
-import {organizationTable} from "@/database/schemas";
+import { db } from '@/database/database'
+import { eq } from 'drizzle-orm'
+import { PostGeoBodyType } from '@/models/geolocation'
+import { organizationTable } from '@/database/schemas'
 
 export async function postGeo(body: PostGeoBodyType, organizationId: string) {
-    const border = body;
+  const border = body.length === 0 ? null : body
 
-    return db.update(organizationTable)
-        .set({border})
-        .where(eq(organizationTable.organizationId, organizationId))
-        .returning({id :organizationTable.organizationId});
+  return db
+    .update(organizationTable)
+    .set({ border })
+    .where(eq(organizationTable.organizationId, organizationId))
+    .returning({ id: organizationTable.organizationId })
 }
 
 export async function getGeo(organizationId: string) {
-    const border = await db.query.organizationTable.findFirst({
-        columns: {
-            border: true,
-        },
-        where: eq(organizationTable.organizationId, organizationId)
-    })
-    return border?.border ?? [];
+  const border = await db.query.organizationTable.findFirst({
+    columns: {
+      border: true,
+    },
+    where: eq(organizationTable.organizationId, organizationId),
+  })
+  return border?.border ?? []
 }
